25d35668938cc8ba2106154872966d34f07bfc79
Caio Séjour <caiosejour@gmail.com>
2025-07-19 14:46:41 -0300

    Alterados alguns detalhes visuais no modal de visualizar tarefa

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import ViewTaskContent from "./components/ViewTaskContent"
 
 import { useState } from 'react'
 import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
-import { RectangleStackIcon, ViewfinderCircleIcon } from '@heroicons/react/24/outline'
+import { RectangleStackIcon, ViewfinderCircleIcon, TrashIcon, PencilSquareIcon } from '@heroicons/react/24/outline'
 
 import { PlusIcon } from '@heroicons/react/24/solid'
 
@@ -186,23 +186,7 @@ export default function Home(){
 
               </div>
 
-              <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                
-                <button
-                  type="button"
-                  onClick={() => setOpenTask(false)}
-                  className="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-green-500 sm:ml-3 sm:w-auto"
-                >
-                  Concluir
-                </button>
-
-                <button
-                  type="button"
-                  onClick={() => setOpenTask(false)}
-                  className="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-blue-500 sm:ml-3 sm:w-auto"
-                >
-                  Editar
-                </button>
+              <div className="bg-gray-50 px-4 py-3 flex justify-between sm:px-6">
                 
                 <button
                   type="button"
@@ -210,9 +194,46 @@ export default function Home(){
                   onClick={() => setOpenTask(false)}
                   className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50 sm:mt-0 sm:w-auto"
                 >
+
                   Cancelar
+
                 </button>
 
+                <div className=" flex">
+
+                  <button
+                    type="button"
+                    onClick={() => setOpenTask(false)}
+                    className="inline-flex w-full justify-center rounded-md border border-red-600 px-3 py-2 text-sm font-semibold text-red-600 shadow-xs hover:bg-red-600 hover:text-white sm:ml-3 sm:w-auto"
+                  >
+
+                    <TrashIcon aria-hidden="true" className="size-5" />
+
+                  </button>
+
+                  <button
+                    type="button"
+                    onClick={() => setOpenTask(false)}
+                    className="inline-flex w-full justify-center rounded-md border border-blue-600 text-blue-600 px-3 py-2 text-sm font-semibold shadow-xs hover:bg-blue-600 hover:text-white sm:ml-3 sm:w-auto"
+                  >
+
+                    <PencilSquareIcon aria-hidden="true" className="size-5" />
+
+                  </button>
+
+                  <button
+                    type="button"
+                    onClick={() => setOpenTask(false)}
+                    className="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-green-500 sm:ml-3 sm:w-auto"
+                  >
+
+                    Concluir tarefa
+
+                  </button>
+
+                </div>
+                
+                
               </div>
 
             </DialogPanel>
